476a95aa59367cd00d336e2a802b77655cca8f74
Alejandro Biancucci <32583931+abiancu@users.noreply.github.com>
2023-01-28 19:18:20 -0600

    Feature/ci with dockerimage (#5)
    * Add CI workflow
    
    * Push image to docker repo
    
    * remove depth filter
    
    * change version number
    
    * change version back and image name
    
    * pass correct pw to echo
    
    * move login step up
    
    * remove set up env
    
    * adding working directory to docker file
    
    * run update npm packages
    
    * added backend for product and image route

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,3 +1,46 @@
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
 exports.index = (req, res) => {
     res.send("Hello from image generator page");
+}
+
+exports.generateImage = async(req, res, next) => {
+
+    try {
+        const response = await openai.createImage({
+            prompt: "A cartoon image of a happy kid driving a formula one race car",
+            n: 1,
+            size: '512x512',
+            response_format: 'url'    
+        })
+
+        res.status(200).json({
+            success: true,
+            body: response.data.data[0].url
+        })
+
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+            res.status(400).json({
+                success: false, 
+                error: error
+            });
+            return next(error);
+        } else {
+            console.log(error.message);
+            res.status(400).json({
+                success: false, 
+                error: error
+            });
+            return next(error);
+        }
+    }
 }
